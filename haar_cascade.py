64c65a37a522aed4b32fd2bb1be1328515fbff45
Abud <abdulrazakdaas0@gmail.com>
2023-07-24 13:18:13 +0300

    better syntax pylint

--- haar_cascade.py
+++ haar_cascade.py
@@ -7,24 +7,22 @@ face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_default.xml')
 def detect(gray ,image):
     faces =face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3,minSize=(3, 3))
     # print(faces)
-    for(x,y ,w ,h) in faces:
-        cv2.rectangle(image,(x,y),(x+w ,y+h),(255,0,0),2) 
+    for(x,y,w,h) in faces:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
     return image
 
 def detect_face(img_path):
-    """
-    """
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
     k=15
-    gray = cv2.GaussianBlur(gray, (k, k), 0)
-    canvas = detect(gray,img)
+    gray=cv2.GaussianBlur(gray,(k,k),0)
+    canvas=detect(gray,img)
 
-    cv2.namedWindow("out", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("out",cv2.WINDOW_NORMAL)
     cv2.imshow('out',canvas)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
  
 if __name__=="__main__":
-    im_path = "img/faces.jpg"
+    im_path="img/faces.jpg"
     detect_face(im_path)
--- mp_face_detection.py
+++ mp_face_detection.py
@@ -15,68 +15,64 @@ FONT_THICKNESS = 1
 TEXT_COLOR = (255, 0, 0)
 
 
+# Checks if the float value is between 0 and 1.
+def is_valid_normalized_value(value: float) -> bool:
+      return (value > 0 or math.isclose(0, value)) and (value < 1 or math.isclose(1, value))
+
 def _normalized_to_pixel_coordinates(
     normalized_x: float, normalized_y: float, image_width: int,
     image_height: int) -> Union[None, Tuple[int, int]]:
-  """Converts normalized value pair to pixel coordinates."""
-
-  # Checks if the float value is between 0 and 1.
-  def is_valid_normalized_value(value: float) -> bool:
-    return (value > 0 or math.isclose(0, value)) and (value < 1 or
-                                                      math.isclose(1, value))
-
-  if not (is_valid_normalized_value(normalized_x) and
-          is_valid_normalized_value(normalized_y)):
-    # TODO: Draw coordinates even if it's outside of the image bounds.
-    return None
-  x_px = min(math.floor(normalized_x * image_width), image_width - 1)
-  y_px = min(math.floor(normalized_y * image_height), image_height - 1)
-  return x_px, y_px
+    """Converts normalized value pair to pixel coordinates."""
+    if not (is_valid_normalized_value(normalized_x) and
+            is_valid_normalized_value(normalized_y)):
+        # TODO: Draw coordinates even if it's outside of the image bounds.
+        return None
+    x_px = min(math.floor(normalized_x * image_width), image_width - 1)
+    y_px = min(math.floor(normalized_y * image_height), image_height - 1)
+    return x_px, y_px
 
 def visualize(
     image,
     detection_result
 ) -> np.ndarray:
-  """Draws bounding boxes and keypoints on the input image and return it.
-  Args:
+    """Draws bounding boxes and keypoints on the input image and return it.
+    Args:
     image: The input RGB image.
     detection_result: The list of all "Detection" entities to be visualize.
-  Returns:
+    Returns:
     Image with bounding boxes.
-  """
-  annotated_image = image.copy()
-  height, width, _ = image.shape
+    """
+    annotated_image = image.copy()
+    height, width, _ = image.shape
 
-  for detection in detection_result.detections:
-    # Draw bounding_box
-    bbox = detection.bounding_box
-    start_point = bbox.origin_x, bbox.origin_y
-    end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
-    cv2.rectangle(annotated_image, start_point, end_point, TEXT_COLOR, 3)
+    for detection in detection_result.detections:
+        # Draw bounding_box
+        bbox = detection.bounding_box
+        start_point = bbox.origin_x, bbox.origin_y
+        end_point = bbox.origin_x + bbox.width, bbox.origin_y + bbox.height
+        cv2.rectangle(annotated_image, start_point, end_point, TEXT_COLOR, 3)
 
-    # Draw keypoints
-    for keypoint in detection.keypoints:
-      keypoint_px = _normalized_to_pixel_coordinates(keypoint.x, keypoint.y,
-                                                     width, height)
-      color, thickness, radius = (0, 255, 0), 2, 2
-      cv2.circle(annotated_image, keypoint_px, thickness, color, radius)
+        # Draw keypoints
+        for keypoint in detection.keypoints:
+            keypoint_px = _normalized_to_pixel_coordinates(keypoint.x, keypoint.y,
+                                                            width, height)
+            color, thickness, radius = (0, 255, 0), 2, 2
+            cv2.circle(annotated_image, keypoint_px, thickness, color, radius)
 
-    # Draw label and score
-    category = detection.categories[0]
-    category_name = category.category_name
-    category_name = '' if category_name is None else category_name
-    probability = round(category.score, 2)
-    result_text = category_name + ' (' + str(probability) + ')'
-    text_location = (MARGIN + bbox.origin_x,
-                     MARGIN + ROW_SIZE + bbox.origin_y)
-    cv2.putText(annotated_image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
-                FONT_SIZE, TEXT_COLOR, FONT_THICKNESS)
+            # Draw label and score
+            category = detection.categories[0]
+            category_name = category.category_name
+            category_name = '' if category_name is None else category_name
+            probability = round(category.score, 2)
+            result_text = category_name + ' (' + str(probability) + ')'
+            text_location = (MARGIN + bbox.origin_x,
+                                MARGIN + ROW_SIZE + bbox.origin_y)
+            cv2.putText(annotated_image, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                        FONT_SIZE, TEXT_COLOR, FONT_THICKNESS)
 
-  return annotated_image
+    return annotated_image
 
 def detect_face(img_path):
-    """
-    """
     # STEP 3: Load the input image.
     image = mp.Image.create_from_file(img_path)
 
@@ -86,7 +82,7 @@ def detect_face(img_path):
        base_options=base_options,
        min_detection_confidence=0.1,
        min_suppression_threshold=0.1
-       )
+    )
     detector = vision.FaceDetector.create_from_options(options)
 
     # STEP 4: Detect faces in the input image.
